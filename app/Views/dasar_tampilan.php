0af8e808073411b069a9695d0717c1191570b896
12200169RezaSaputra <sorakairi1909@gmail.com>
2021-10-05 13:51:49 +0700

    Tugas Pertemuan 3

---
+++ app/Views/dasar_tampilan.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <tittle></tittle>
+        <link href="<?=base_url('/assets/css/bootstrap.min.css')?>" rel="stylesheet"  />
+</head>
+<body>
+
+        <?=$this->renderSection('isiweb')?>
+
+        <script src="<?=base_url('/assets/js/bootstrap.bundle.min.js')?>"></script>
+</body>
+
+</html>
